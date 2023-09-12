90638b534ab037700213938ec7d2f82ba154e05a
juanyonda <juan.yonda@misena.edu.co>
2023-09-12 11:24:36 -0500

    felipe

---
+++ src/test/java/com/co/bonbonite/runners/ShoesRunner.java
@@ -0,0 +1,9 @@
+package com.co.bonbonite.runners;
+
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+
+public class ShoesRunner {
+}
